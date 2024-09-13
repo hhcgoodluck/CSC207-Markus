d3529e9af3d7da1ac3798d51f640cd8adb27c776
Hongcheng Huo <hongcheng.huo@mail.utoronto.ca>
2024-09-13 16:25:06 -0400

    works

--- week1git/src/Reduce.java
+++ week1git/src/Reduce.java
@@ -1,5 +1,6 @@
 public class Reduce {
-    public static int countSteps(int n) {
+    // 计算将 n 减少到 0 的步骤数
+    public static int calculateSteps(int n) {
         int steps = 0;
         while (n > 0) {
             if (n % 2 == 0) {
@@ -12,13 +13,21 @@ public class Reduce {
         return steps;
     }
 
+    // 处理命令行参数的 main 方法
     public static void main(String[] args) {
-        int n = 100; // 默认值
 
+        int defaultN = 100;
+
+        // 处理命令行参数
+        int n = defaultN;
         if (args.length > 0) {
-            n = Integer.parseInt(args[0]);
+            try {
+                n = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Using default value.");
+            }
         }
 
-        System.out.println("Number of steps to reduce " + n + " to 0: " + countSteps(n));
+        System.out.println(calculateSteps(n));
     }
 }
